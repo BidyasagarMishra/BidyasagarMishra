abf8df63adde7614ce71ee61d0395dd9ca5a7849
Bidyasagar Mishra <bidyacodefor.net@hotmail.com>
2023-03-17 15:38:11 +0530

    Here I learnt how to assign multiple values

---
+++ MultipleVariables.py
@@ -0,0 +1,22 @@
+#many values to multiple variables
+print('many values to multiple variables')
+x,y,z="Orange","kiwi","Berry"
+print(x)
+print(y)
+print(z)
+
+#single values to multiple variables
+print('single values to multiple variables')
+a=b=c="Mango"
+print(a)
+print(b)
+print(c)
+
+#Unpack a Collection
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+print("unpack a collection")
+fruits=["apple","banana","cherry"]
+d,e,f=fruits
+print(d)
+print(e)
+print(f)
