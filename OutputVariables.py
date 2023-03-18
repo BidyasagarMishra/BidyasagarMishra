26a51f3dbba7e41848b5a9463fcbf86ad0d2184e
Bidyasagar Mishra <bidyacodefor.net@hotmail.com>
2023-03-18 23:12:19 +0530

    In this tutorial I learn how to concatenate 2 strings and 2 integers

---
+++ OutputVariables.py
@@ -0,0 +1,22 @@
+#The Python print() function is often used to output variables.
+
+#Only one string
+print("Only one string")
+x="This is a string"
+print(x)
+
+#Only one integer
+print("Only one integer")
+y=4
+print(y)
+
+
+#two combined string
+print("two combined string")
+z="this is the 2nd string"
+print(x,z) #if we will use the + symbol then it will provide an error 
+
+#additon of 2 integers
+print("addition of integers")
+a=4
+print(a+y)
